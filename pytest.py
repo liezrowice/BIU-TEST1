244726293525a57cc0a67ae68b93c4168c264199
Daniel Liezrowice <49018183+liezrowice@users.noreply.github.com>
2024-10-12 20:07:20 +0300

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Ask the user for input
-user_input = input("Enter something: ")
+user_input = input("Enter a text string: ")
 
 # Print the input back to the screen
 print("You entered:", user_input)
